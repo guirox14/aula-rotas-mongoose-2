d9fc9ee969a4c758cc440b711915daf1bd4f0308
Guilherme Senger <guilherme.gui1234@gmail.com>
2015-10-24 11:48:01 -0200

    Aula

---
+++ app.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var Controller = require('./controller');
+
+http.createServer(function (req, res) {
+	console.log('url: ', req.url);
+	var url = req.url;
+	
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	switch(url) {
+		case '/create':
+			Controller.create(req, res);
+			break;
+		case '/retrieve':
+			Controller.retrieve(req, res);
+			break;
+		case '/update':
+			Controller.update(req, res);
+			break;
+		case '/delete':	
+			Controller.delete(req, res);
+			break;
+		default:
+			console.log("Nada a fazer");
+	}
+	
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
